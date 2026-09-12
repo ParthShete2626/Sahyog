3b4dcc90f67b6621a85dae1b620ff5241e89c671
ParthShete2626 <parthshete2626@gmail.com>
2026-09-12 19:29:22 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    netlify(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+});
